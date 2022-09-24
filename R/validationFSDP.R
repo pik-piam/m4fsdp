7043c9db9ffa57b6cd2295fbe10ad7e923b59529
florianh <humpenoeder@pik-potsdam.de>
2022-09-24 14:40:01 +0200

    validation

--- R/validationFSDP.R
+++ R/validationFSDP.R
@@ -81,31 +81,28 @@ validationFSDP <- function(repReg, valfile, folder = "output") {
   val$region_class <- factor(val$region_class, levels = regSubOrder)
 
   # plot function
-  plotVal <- function(var, units = NULL, weight = NULL, hist = NULL) {
+  plotVal <- function(var, units = NULL, varName = NULL, unitName = NULL, weight = NULL, hist = NULL, histName = NULL) {
     if (is.null(units)) {
       units <- levels(rep$unit)
     }
     b <- rep[rep$variable == var & rep$unit %in% units & rep$region_class != "GLO", ]
     b <- droplevels(b)
     units <- levels(b$unit)
-    unitHist <- levels(val$unit)[grep(units, levels(val$unit), fixed = TRUE)]
+    unitHist <- levels(val$unit)[grep(units, levels(val$unit), fixed = TRUE)][1]
     if (is.null(hist)) {
       h <- val[val$variable == var & val$unit == unitHist & val$region_class != "GLO" & val$scenario == "historical" &
                  val$period >= 1980 & val$period <= 2020, ]
     } else {
       h <- val[val$variable == var & val$unit == unitHist & val$region_class != "GLO" & val$scenario == "historical" &
                  val$period >= 1980 & val$period <= 2020 & val$model %in% hist, ]
+      h <- droplevels(h)
+      if (!is.null(histName)) levels(h$model) <- histName
     }
 
     if (!is.null(weight)) {
       w1 <- rep[rep$variable == weight & rep$region_class != "GLO", ]
-      if (is.null(hist)) {
-        w2 <- val[val$variable == weight & val$region_class != "GLO" & val$scenario == "historical" &
-                    val$period >= 1980 & val$period <= 2020, ]
-      } else {
-        w2 <- val[val$variable == weight & val$region_class != "GLO" & val$scenario == "historical" &
-                    val$period >= 1980 & val$period <= 2020 & val$model %in% hist, ]
-      }
+      w2 <- val[val$variable == weight & val$region_class != "GLO" & val$scenario == "historical" &
+                  val$period >= 1980 & val$period <= 2020, ]
       b <- cbind(b, w1$value)
       h <- cbind(h, w2$value)
       b <- b[, list(value = weighted.mean(get("value"), get("V2"))),
@@ -113,15 +110,17 @@ validationFSDP <- function(repReg, valfile, folder = "output") {
       h <- h[, list(value = weighted.mean(get("value"), get("V2"))),
              by = c("region_class", "model", "scenario", "variable", "unit", "period")]
     } else {
-      saveRDS(b, "b.rds")
       b <- b[, list(value = sum(get("value"))),
              by = c("region_class", "model", "scenario", "variable", "unit", "period")]
       h <- h[, list(value = sum(get("value"))),
              by = c("region_class", "model", "scenario", "variable", "unit", "period")]
     }
 
+    if (is.null(varName)) varName <- var
+    if (is.null(unitName)) unitName <- units
+
     p <- ggplot(b, aes_string(x = "period", y = "value"))
-    p <- p + labs(title = var) + ylab(units) + xlab(NULL) + themeMy(rotateX = 90)
+    p <- p + labs(title = varName) + ylab(unitName) + xlab(NULL) + themeMy(rotateX = 90)
     if (nrow(h) > 0) p <- p + geom_point(data = h, aes_string(shape = "model"))
     p <- p + geom_line(aes_string(color = "scenario")) + facet_wrap("region_class")
     p <- p + scale_shape_discrete("Historical data", solid = 0)
@@ -130,15 +129,56 @@ validationFSDP <- function(repReg, valfile, folder = "output") {
                     shape = guide_legend(order = 2, title.position = "top"))
   }
 
+  # Validation assumptions
   p1 <- plotVal(var = "Population")
-  p2 <- plotVal(var = "Income", units = "US$05 PPP/cap/yr", weight = "Population")
-  p3 <- plotVal(var = "Nutrition|Calorie Supply|+|Crops", weight = "Population")
-  p4 <- plotVal(var = "Nutrition|Calorie Supply|+|Livestock products", weight = "Population")
-  p5 <- plotVal(var = "Demand|++|Crops")
-  p6 <- plotVal(var = "Demand|++|Livestock products")
+  p2 <- plotVal(var = "Income", units = "US$05 PPP/cap/yr", varName = "Per-capita income", weight = "Population")
+  p3 <- plotVal(var = "Nutrition|Calorie Supply|+|Crops", weight = "Population",
+                varName = "Per-capita calorie supply from crops", hist = "FAOmassbalance")
+  p4 <- plotVal(var = "Nutrition|Calorie Supply|+|Livestock products",
+                varName = "Per-capita calorie supply from livestock products",
+                weight = "Population", hist = "FAOmassbalance")
+  p5 <- plotVal(var = "Demand|++|Crops", varName = "Total crop demand including feed and feed")
+  p6 <- plotVal(var = "Demand|++|Livestock products", varName = "Total livestock product demand")
 
   combined <- p1 + p2 + p3 + p4 + p5 + p6 + plot_annotation(tag_levels = "a")
   combined <- combined + plot_layout(guides = "keep", ncol = 2) & theme(legend.position = "bottom")
   ggsave(filename = file.path(folder, paste(rev, "valAssumptions.png", sep = "_")), combined,
          width = 10, height = 10, scale = 1.3)
+
+  # Validation land
+  p1 <- plotVal(var = "Resources|Land Cover|+|Cropland", varName = "Land Cover|Cropland", hist = "FAO_crop_past")
+  p2 <- plotVal(var = "Resources|Land Cover|+|Pastures and Rangelands",
+                varName = "Land Cover|Pastures and Rangelands", hist = "FAO_crop_past")
+  p3 <- plotVal(var = "Resources|Land Cover|Forest|+|Managed Forest",
+                varName = "Managed forest incl. afforestation", hist = "MAgPIEown", histName = "Based on LUH2")
+  p4 <- plotVal(var = "Resources|Land Cover|Forest|+|Natural Forest",
+                varName = "Primary and secondary forest", hist = "MAgPIEown", histName = "Based on LUH2")
+  p5 <- plotVal(var = "Resources|Land Cover|+|Other Land",
+                varName = "Other natural land", hist = "MAgPIEown", histName = "Based on LUH2")
+  p6 <- plotVal(var = "Resources|Land Cover|+|Urban Area",
+                varName = "Urban land", hist = "MAgPIEown", histName = "Based on LUH2")
+
+  combined <- p1 + p2 + p3 + p4 + p5 + p6 + plot_annotation(tag_levels = "a")
+  combined <- combined + plot_layout(guides = "keep", ncol = 2) & theme(legend.position = "bottom")
+  ggsave(filename = file.path(folder, paste(rev, "valLand.png", sep = "_")), combined,
+         width = 10, height = 10, scale = 1.3)
+
+  # Validation Yields and TC
+  p1 <- plotVal(var = "Productivity|Landuse Intensity Indicator Tau", varName = "Landuse Intensity Indicator Tau")
+  p2 <- plotVal(var = "Resources|Nitrogen|Cropland Budget|Inputs|+|Fertilizer",
+                varName = "Sythetic nitrogen fertilizer", hist = "Bodirsky")
+  p3 <- plotVal(var = "Productivity|Yield|Crops|+|Cereals",
+                varName = "Cereal crop yields")
+  p4 <- plotVal(var = "Productivity|Yield|Crops|+|Sugar crops",
+                varName = "Sugar crop yields")
+  p5 <- plotVal(var = "Productivity|Yield|Crops|+|Oil crops",
+                varName = "Oil crop yields")
+  p6 <- plotVal(var = "Productivity|Yield|+|Pasture",
+                varName = "Pasture yields")
+
+  combined <- p1 + p2 + p3 + p4 + p5 + p6 + plot_annotation(tag_levels = "a")
+  combined <- combined + plot_layout(guides = "keep", ncol = 2) & theme(legend.position = "bottom")
+  ggsave(filename = file.path(folder, paste(rev, "valYield.png", sep = "_")), combined,
+         width = 10, height = 10, scale = 1.3)
+
 }
