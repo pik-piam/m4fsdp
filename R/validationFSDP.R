208f831afb0fb898338af9c1649843211178d12d
Benjamin Leon Bodirsky <bodirsky@pik-potsdam.de>
2022-12-02 16:02:02 +0800

    improving validation

--- R/validationFSDP.R
+++ R/validationFSDP.R
@@ -14,14 +14,26 @@
 #' @importFrom utils write.csv
 #' @importFrom stats reorder
 
-validationFSDP <- function(repReg, val, regionSel = "aggregate", folder = "output") {
+validationFSDP <- function(repReg, val, regionSel = "aggregate", folder = "output", ssps=FALSE) {
 
   #### read in data files
-  rep <- convertReportFSDP(repReg, scengroup = c("FSECc", "FSECe"), subset = FALSE)
+  if(ssps){
+    rep <- convertReportFSDP(repReg, scengroup = c("FSECc", "FSECd","FSECe"), subset = FALSE)
+    #rep <- rep[rep$scenario %in%c("SSP1bau","SSP1PLUSbau", "SSP2bau","SSP2fsdp","SSP3bau","SSP4bau", "SSP5bau", "FSDP"), ]
+    rep <- rep[rep$scenario %in%c("SSP1bau","SSP1PLUSbau", "SSP2bau", "SSP5bau", "FSDP"), ]
+  } else {
+    rep <- convertReportFSDP(repReg, scengroup = c("FSECc","FSECe"), subset = FALSE)
+  }
+
   rev <- levels(rep$version)
   rep$scenset <- NULL
-  if (!is.data.frame(val)) val <- readRDS(val)
-  val[region == "World", region := "GLO"]
+  if (!is.data.frame(val)) valdata <- readRDS(val)
+  valdata[region == "World", region := "GLO"]
+
+  safe_colorblind_palette <- c("#88CCEE", "#CC6677", "#DDCC77", "#332288", "#AA4499",
+                               "#44AA99", "#999933", "#882255", "#661100", "#6699CC", "#888888")
+  fsec <- c("#88CCEE", "#CC6677", "#DDCC77", "#332288", "#AA4499",
+                               "#44AA99", "#999933", "#882255", "#661100", "#6699CC", "#888888")
 
   themeMy <- function(baseSize = 11, baseFamily = "", rotateX = FALSE, panelSpacing = 3) {
     txt <- element_text(size = baseSize, colour = "black", face = "plain")
@@ -74,71 +86,87 @@ validationFSDP <- function(repReg, val, regionSel = "aggregate", folder = "outpu
     map[14, ] <- c("USA", "HIR")
     map[15, ] <- c("GLO", "GLO")
     rep <- merge(rep, map)
-    val <- merge(val, map)
+    valdata <- merge(valdata, map)
 
     regSubOrder <- c("LIR", "MIR", "HIR", "GLO")
     rep$region_class <- factor(rep$region_class, levels = regSubOrder)
-    val$region_class <- factor(val$region_class, levels = regSubOrder)
+    valdata$region_class <- factor(valdata$region_class, levels = regSubOrder)
 
     rep <- rep[rep$region_class != "GLO", ]
-    val <- val[val$region_class != "GLO", ]
+    valdata <- valdata[valdata$region_class != "GLO", ]
 
   } else {
 
     rep$region_class <- rep$region
-    val$region_class <- val$region
+    valdata$region_class <- valdata$region
     rep <- rep[rep$region_class %in% regionSel, ]
-    val <- val[val$region_class %in% regionSel, ]
+    valdata <- valdata[valdata$region_class %in% regionSel, ]
 
   }
 
   # plot function
   plotVal <- function(var, units = NULL, varName = NULL, unitName = NULL, weight = NULL, hist = NULL, histName = NULL) {
-    if (is.null(units)) {
-      units <- levels(rep$unit)
-    }
-    b <- rep[rep$variable == var & rep$unit %in% units, ]
-    b <- droplevels(b)
-    units <- levels(b$unit)
-    unitHist <- levels(val$unit)[grep(units, levels(val$unit), fixed = TRUE)][1]
-    if (is.null(hist)) {
-      h <- val[val$variable == var & val$unit == unitHist & val$scenario == "historical" &
-                 val$period >= 1980 & val$period <= 2020, ]
+    empty2null<-function(x){out<-x; if(!is.null(x)){if(x=="empty"){out<-NULL}}; return(out)}
+    varName=empty2null(varName)
+    weight=empty2null(weight)
+    hist=empty2null(hist)
+    units=empty2null(units)
+    unitName=empty2null(unitName)
+    histName=empty2null(histName)
+
+    if (var %in% rep$variable){
+      if (is.null(units)) {
+        units <- levels(rep$unit)
+      }
+      b <- rep[rep$variable == var & rep$unit %in% units & rep$period >= 1995 & rep$period <= 2050, ]
+      b <- droplevels(b)
+      units <- levels(b$unit)
+      unitHist <- levels(valdata$unit)[grep(units, levels(valdata$unit), fixed = TRUE)][1]
+      if (is.null(hist)) {
+        h <- valdata[valdata$variable == var & valdata$unit == unitHist & valdata$scenario == "historical" &
+                   valdata$period >= 1980 & valdata$period <= 2020, ]
+      } else {
+        h <- valdata[valdata$variable == var & valdata$unit == unitHist & valdata$scenario == "historical" &
+                   valdata$period >= 1980 & valdata$period <= 2020 & valdata$model %in% hist, ]
+        h <- droplevels(h)
+        if (!is.null(histName)) levels(h$model) <- histName
+      }
+
+      if (!is.null(weight)) {
+        w1 <- rep[rep$variable == weight, ]
+        w2 <- valdata[valdata$variable == weight & valdata$scenario == "historical" &
+                    valdata$period >= 1980 & valdata$period <= 2020, ]
+        b <- cbind(b, w1$value)
+        h <- cbind(h, w2$value)
+        b <- b[, list(value = weighted.mean(get("value"), get("V2"))),
+               by = c("region_class", "model", "scenario", "variable", "unit", "period")]
+        h <- h[, list(value = weighted.mean(get("value"), get("V2"))),
+               by = c("region_class", "model", "scenario", "variable", "unit", "period")]
+      } else {
+        b <- b[, list(value = sum(get("value"))),
+               by = c("region_class", "model", "scenario", "variable", "unit", "period")]
+        h <- h[, list(value = sum(get("value"))),
+               by = c("region_class", "model", "scenario", "variable", "unit", "period")]
+      }
+
+      if (is.null(varName)) varName <- var
+      if (is.null(unitName)) unitName <- units
+
+      p <- ggplot(b, aes_string(x = "period", y = "value"))
+      p <- p + labs(title = varName) + ylab(unitName) + xlab(NULL) + themeMy(rotateX = 90)
+      if (nrow(h) > 0) p <- p + geom_point(data = h, aes_string(shape = "model"))
+      p <- p + geom_line(aes_string(color = "scenario")) + facet_wrap("region_class")
+      p <- p + scale_shape_discrete("Historical data", solid = 0)
+      #p <- p + scale_color_brewer("MAgPIE scenario", palette = "Set2")
+      p <- p + scale_color_manual("MAgPIE scenario", values = safe_colorblind_palette)
+      p <- p + guides(color = guide_legend(order = 1, title.position = "top"),
+                      shape = guide_legend(order = 2, title.position = "top"))
+      return(p)
     } else {
-      h <- val[val$variable == var & val$unit == unitHist & val$scenario == "historical" &
-                 val$period >= 1980 & val$period <= 2020 & val$model %in% hist, ]
-      h <- droplevels(h)
-      if (!is.null(histName)) levels(h$model) <- histName
+      warning(paste0("Missing Variable: ",var))
+      return(NULL)
     }
 
-    if (!is.null(weight)) {
-      w1 <- rep[rep$variable == weight, ]
-      w2 <- val[val$variable == weight & val$scenario == "historical" &
-                  val$period >= 1980 & val$period <= 2020, ]
-      b <- cbind(b, w1$value)
-      h <- cbind(h, w2$value)
-      b <- b[, list(value = weighted.mean(get("value"), get("V2"))),
-             by = c("region_class", "model", "scenario", "variable", "unit", "period")]
-      h <- h[, list(value = weighted.mean(get("value"), get("V2"))),
-             by = c("region_class", "model", "scenario", "variable", "unit", "period")]
-    } else {
-      b <- b[, list(value = sum(get("value"))),
-             by = c("region_class", "model", "scenario", "variable", "unit", "period")]
-      h <- h[, list(value = sum(get("value"))),
-             by = c("region_class", "model", "scenario", "variable", "unit", "period")]
-    }
-
-    if (is.null(varName)) varName <- var
-    if (is.null(unitName)) unitName <- units
-
-    p <- ggplot(b, aes_string(x = "period", y = "value"))
-    p <- p + labs(title = varName) + ylab(unitName) + xlab(NULL) + themeMy(rotateX = 90)
-    if (nrow(h) > 0) p <- p + geom_point(data = h, aes_string(shape = "model"))
-    p <- p + geom_line(aes_string(color = "scenario")) + facet_wrap("region_class")
-    p <- p + scale_shape_discrete("Historical data", solid = 0)
-    p <- p + scale_color_brewer("MAgPIE scenario", palette = "Set2")
-    p <- p + guides(color = guide_legend(order = 1, title.position = "top"),
-                    shape = guide_legend(order = 2, title.position = "top"))
   }
 
   # Validation assumptions
@@ -205,39 +233,92 @@ validationFSDP <- function(repReg, val, regionSel = "aggregate", folder = "outpu
   ggsave(filename = file.path(folder, paste(rev, "valYield.pdf", sep = "_")), combined,
          width = 10, height = 10, scale = 1.3)
 
+  # Validation outcome inidicators:
+
+
+  varname_ind <- list("Health|Prevalence of underweight"=c( #varname
+                          "SDG|SDG02|Prevalence of underweight",#var
+                          "empty", #valdataname
+                          "empty"), #weight
+                   "Health|Prevalence of obesity"=c(
+                     "SDG|SDG03|Prevalence of obesity", #var
+                     "empty", #valdataname
+                     "empty"), #weight
+                   "Health|Years of life lost"=c(
+                     "Health|Years of life lost|Risk|Diet and anthropometrics", #var
+                     "empty", #valdataname
+                     "empty"), #weight
+                   "Environment|Biodiversity Intactness"=c(
+                     "Biodiversity|BII", #var
+                     "Phillips et al", #valdataname
+                     "Resources|Land Cover"), #weight
+                   "Environment|Shannon crop area diversity index"=c(
+                     "Biodiversity|Shannon crop area diversity index", #var
+                     "empty", #valdataname
+                     "empty"), #weight
+                   "Environment|Nitrogen surplus"=c(
+                     "Resources|Nitrogen|Nutrient surplus incl natural vegetation", #var
+                     "MADRaT", #valdataname
+                     "empty"), #weight
+                   "Environment|Water environmental flow violations"=c(
+                     "Water|Environmental flow violation volume", #var
+                     "empty", #valdataname
+                     "empty"), #weight
+                   "Environment|Cumulative GHG emissions"=c(
+                     "Emissions|GWP100AR6|Land|Cumulative", #var
+                     "empty", #valdataname
+                     "empty"), #weight
+                   "Environment|Global Surface Temperature"=c(
+                     "Global Surface Temperature", #var
+                     "GISTEMP", #valdataname
+                     "empty"), #weight
+                   "Inclusion|Expenditure for agric. products"=c(
+                     "Household Expenditure|Food|Expenditure", #var
+                     "empty", #valdataname
+                     "empty"), #weight
+                   "Inclusion|Number of People Below 3.20$/Day"=c(
+                     "Number of People Below 3.20$/Day", #var
+                     "empty", #valdataname
+                     "empty"), #weight
+                   "Inclusion|Agricultural employment"=c(
+                     "Agricultural employment|Crop and livestock products", #var
+                     "empty", #valdataname
+                     "empty"), #weight,
+                   "Inclusion|Agricultural wages"=c(
+                     "Hourly labor costs relative to 2020", #var
+                     "empty", #valdataname
+                     "empty"), #weight,
+                   "Economy|Bioeconomy Supply"=c(
+                     "Value|Bioeconomy Demand",#var
+                     "empty", #valdataname
+                     "empty"), #weight
+                   "Economy|Costs"=c(
+                     "Costs Without Incentives", #var
+                     "empty", #valdataname
+                     "empty") #weight
+  )
+
+  names(varname_ind)<-gsub(pattern = "\\|",replacement = "\n",x = names(varname_ind))
 
   # BII
-  p1 <- plotVal(var = "Biodiversity|BII",
-                varName = "Biodiversity Intactness Index",
-                hist = "Phillips et al",
-                weight = "Resources|Land Cover")
-
-  ggsave(filename = file.path(folder, paste(rev, "valBII.png", sep = "_")), p1,
-         width = 10, height = 10, scale = 1.3)
-  ggsave(filename = file.path(folder, paste(rev, "valBII.pdf", sep = "_")), p1,
-         width = 10, height = 10, scale = 1.3)
-
-
-  # Nutrient surplus
-  if ("Resources|Nitrogen|Nutrient surplus from land and manure management" %in% levels(rep$variable)) {
-    p1 <- plotVal(var = "Resources|Nitrogen|Nutrient surplus from land and manure management",
-                  varName = "Nutrient surplus from land and manure management",
-                  hist = "MADRaT")
-
-    ggsave(filename = file.path(folder, paste(rev, "valNutrientSurplus.png", sep = "_")), p1,
-           width = 10, height = 10, scale = 1.3)
-    ggsave(filename = file.path(folder, paste(rev, "valNutrientSurplus.pdf", sep = "_")), p1,
-           width = 10, height = 10, scale = 1.3)
-  } else {
-    warning("Missing Variable: Resources|Nitrogen|Nutrient surplus from land and manure management")
+  combined=NULL
+  for (count in 1:length(varname_ind)){
+    tmp <- plotVal(var = varname_ind[[count]][[1]],
+                varName = names(varname_ind[count]),
+                hist = varname_ind[[count]][[2]],
+                weight = varname_ind[[count]][[3]])
+    if(!is.null(tmp)){
+      if(!is.null(combined)){
+        combined <- combined+tmp
+      } else {combined <- tmp}
+    }
   }
 
-
-  # Global Surface Temeprature
-  p1 <- plotVal(var = "Global Surface Temperature", varName = "Global Surface Temperature", hist = "GISTEMP")
-
-  ggsave(filename = file.path(folder, paste(rev, "valGlobalSurfaceTemperature.png", sep = "_")), p1,
+  combined <- combined + plot_annotation(tag_levels = "a")
+  combined <- combined + plot_layout(guides = "collect", ncol = 5) & theme(legend.position = "bottom")
+  ggsave(filename = file.path(folder, paste(rev, "valOutcome.png", sep = "_")), combined,
+         width = 10, height = 8, scale = 1.3)
+  ggsave(filename = file.path(folder, paste(rev, "valOutcome.pdf", sep = "_")), combined,
          width = 10, height = 10, scale = 1.3)
-  ggsave(filename = file.path(folder, paste(rev, "valGlobalSurfaceTemperature.pdf", sep = "_")), p1,
-         width = 10, height = 10, scale = 1.3)
+
 }
