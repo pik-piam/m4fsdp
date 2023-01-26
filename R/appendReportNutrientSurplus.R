fb7fb2d6d6f04cb6d53f0be568213e6b6af41a23
Michael Crawford <mscrawfor@gmail.com>
2023-01-26 18:01:15 +0100

    Added nutr surplus from non-ag land to report mif and rds

--- R/appendReportNutrientSurplus.R
+++ R/appendReportNutrientSurplus.R
@@ -13,6 +13,7 @@
 #'
 #' @importFrom magclass read.report write.report getItems
 #' @importFrom dplyr %>% mutate select arrange rename
+#' @importFrom purrr map2_df
 #' @importFrom forcats fct_recode
 #' @importFrom quitte as.quitte
 #' @importFrom madrat toolAggregate
@@ -20,41 +21,52 @@
 
 appendReportNutrientSurplus <- function(scenario, dir = ".") {
 
-    # -----------------------------------------------------------------------------------------------------------------
-    # Calculate total global nutrient surplus
+    .generateReport <- function(fileName, variableName) {
 
-    NSgrid_path <- file.path(dir, paste0(scenario, "-nutrientSurplus_total.mz"))
-    NSgrid      <- read.magpie(NSgrid_path)
+        # -----------------------------------------------------------------------------------------------
+        # Calculate total regional and global nutrient surplus from grid-level datasets
 
-    NScountry <- dimSums(NSgrid, dim = 1.2) # Grid -> country level
+        NSgrid_path <- file.path(dir, paste0(scenario, "-nutrientSurplus_", fileName, ".mz"))
+        NSgrid      <- read.magpie(NSgrid_path)
 
-    gdx <- file.path(dir, "fulldata.gdx")
-    mapping <- readGDX(gdx, "i_to_iso")
-    mapping <- mapping %>% mutate(GlobalCode = "World")
+        NScountry <- dimSums(NSgrid, dim = 1.2) # Grid -> country level
 
-    NSregWorld <- toolAggregate(x       = NScountry,
-                                rel     = mapping,
-                                from    = "iso",
-                                to      = "i+GlobalCode",
-                                partrel = TRUE)
+        gdx <- file.path(dir, "fulldata.gdx")
+        mapping <- readGDX(gdx, "i_to_iso")
+        mapping <- mapping %>% mutate(GlobalCode = "World")
 
-    NSregWorld <- NSregWorld %>% as.data.frame()
+        NSregWorld <- toolAggregate(x       = NScountry,
+                                    rel     = mapping,
+                                    from    = "iso",
+                                    to      = "i+GlobalCode",
+                                    partrel = TRUE)
 
-    colnames(NSregWorld) <- c("cell", "region", "year", "variable", "value")
+        NSregWorld <- NSregWorld %>% as.data.frame()
 
-    NSregWorld <- NSregWorld %>%
-        mutate(variable = "Resources|Nitrogen|Nutrient surplus from land and manure management") %>%
-        mutate(model    = "MAgPIE",
-               scenario = scenario,
-               unit     = "Mt N") %>%
-        select(.data$model, .data$scenario, .data$region, .data$variable, .data$unit, .data$year, .data$value) %>%
-        arrange(.data$model, .data$scenario, .data$variable, .data$year, .data$region)
+        colnames(NSregWorld) <- c("cell", "region", "year", "variable", "value")
 
+        NSregWorld <- NSregWorld %>%
+            mutate(variable = variableName) %>%
+            mutate(model    = "MAgPIE",
+                   scenario = scenario,
+                   unit     = "Mt N") %>%
+            select(.data$model, .data$scenario, .data$region, .data$variable, .data$unit, .data$year, .data$value) %>%
+            arrange(.data$model, .data$scenario, .data$variable, .data$year, .data$region)
+    }
 
-    # -----------------------------------------------------------------------------------------------------------------
+    fileNames <- c("nonAgLand", "total")
+
+    variableNames <- c("Resources|Nitrogen|Nutrient surplus from non-agricultural land",
+                       "Resources|Nitrogen|Nutrient surplus from land and manure management")
+
+    surpluses <- purrr::map2_df(.x = fileNames,
+                                .y = variableNames,
+                                .f = .generateReport)
+
+    # -----------------------------------------------------------------------------------------------
     # Append report to report.rds
 
-    NSregWorld_rds <- NSregWorld %>% rename(period = .data$year)
+    surpluses_rds <- surpluses %>% rename(period = .data$year)
 
     rds_file <- file.path(dir, "report.rds")
     if (file.exists(rds_file)) {
@@ -62,11 +74,11 @@ appendReportNutrientSurplus <- function(scenario, dir = ".") {
         report_rds <- readRDS(rds_file)
 
         # If the variables are already included within the report, remove these older variables for replacement
-        if (any(NSregWorld_rds$variable %in% report_rds$variable)) {
-            report_rds <- report_rds %>% filter(!.data$variable %in% NSregWorld_rds$variable)
+        if (any(surpluses_rds$variable %in% report_rds$variable)) {
+            report_rds <- report_rds %>% filter(!.data$variable %in% surpluses_rds$variable)
         }
 
-        report_rds <- rbind(report_rds, NSregWorld_rds)
+        report_rds <- rbind(report_rds, surpluses_rds)
         toSaveAsRDS <- as.quitte(report_rds)
         saveRDS(toSaveAsRDS, file = file.path(dir, "report.rds"), version = 2)
 
@@ -74,26 +86,21 @@ appendReportNutrientSurplus <- function(scenario, dir = ".") {
         stop("report.rds wasn't found for scenario: ", scenario)
     }
 
-
-    # -----------------------------------------------------------------------------------------------------------------
+    # -----------------------------------------------------------------------------------------------
     # Append report to report.mif
 
-    NSregWorld_mif <- NSregWorld %>%
-        mutate(region = fct_recode(.data$region,
-                                   "GLO" = "World"))
+    surpluses_mif <- surpluses %>%
+        mutate(region = fct_recode(.data$region, "GLO" = "World"))
 
-    NSregWorld_mif <- as.magpie(NSregWorld_mif)
+    surpluses_mif <- as.magpie(surpluses_mif)
 
     mif_file <- file.path(dir, "report.mif")
     if (file.exists(mif_file)) {
 
-        originalReport <- read.report(mif_file, as.list = FALSE)
+        originalReport      <- read.report(mif_file, as.list = FALSE)
         originalReportItems <- getItems(originalReport, dim = 3.3)
-        newReportItems <- getItems(NSregWorld_mif, dim = 3.3)
+        newReportItems      <- getItems(surpluses_mif, dim = 3.3)
 
-        # If the variables are already included within the report, remove these older variables for replacement
-        # This is currently unnecessarily complex, given that there is only one reporting variable (Nutrient surplus
-        # incl natural vegetation), but will be helpful if further reporting names are added.
         alreadyPresentItems <- Map(newReportItems,
                                    f = function(.x) grepl(pattern = .x, x = originalReportItems, fixed = TRUE))
         alreadyPresentItems <- Reduce(alreadyPresentItems, f = `|`)
@@ -103,7 +110,7 @@ appendReportNutrientSurplus <- function(scenario, dir = ".") {
         }
 
         write.report(x = originalReport, file = mif_file)
-        write.report(x = NSregWorld_mif, file = mif_file, append = TRUE)
+        write.report(x = surpluses_mif, file = mif_file, append = TRUE)
 
     } else {
         stop("report.mif wasn't found for scenario: ", scenario)
